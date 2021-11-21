3673b891c2b9498167178c671de2597529385da6
Matt Crabtree <m.crabtree@zohomail.eu>
2021-11-21 19:56:42 +0000

    functions created, backup working

--- shuffler.py
+++ shuffler.py
@@ -1,58 +1,37 @@
-#!/bin/python3
+#!python3
 # shuffler.py
-"""feed this script a text file with spotify tracks and it'll spit out a randomised tracklist"""
 
-# import sys
 import random
 import pyperclip as pc
 
-output = ""
-tracklist = pc.paste()
-urls = tracklist.split("\n")
-random.shuffle(urls)
 
-for url in urls:
-    output += url + "\n"
-
-# pc.copy(output)
+def handle_input():
+    tracklist = pc.paste()
+    urls = tracklist.split("\n")
+    random.shuffle(urls)
+    return urls
 
 
-# def file_check(file_path):
-#     line_count = 0
-#     test_string = "https://open.spotify.com/track/"
-#     with open(file_path) as file:
-#         for line in file:
-#             track_id = line[-22:].strip()
-#             line_count += 1
-#             if line.lower().startswith(test_string) and track_id.isalnum():
-#                 pass
-#             else:
-#                 raise Exception(f'error, line {str(line_count)}: "{line}" unsupported')
+def handle_output(tracklist):
+    output = ""
+    count = 0
+    for track in tracklist:
+        if not track.strip():
+            continue
+        else:
+            output += track + "\n"
+            count += 1
 
-# def read_tracklist(file_path):
-#     tracklist = []
-#     with open(file_path) as file:
-#         for line in file:
-#             tracklist.append(line.strip())
-#     return tracklist
+    print(f"successfully shuffled {count} tracks!")
+    pc.copy(output)
 
 
-# def write_tracklist(tracklist, filename):
-#     with open(filename, "w") as file:
-#         for track in tracklist:
-#             file.write(f"{track}\n")
+def backup(data):
+    with open(".backup", "w") as f:
+        for line in data:
+            f.write(line + "\n")
 
 
-# def read_clipboard():
-#     return pc.paste()
-
-# file_path = sys.argv[1]
-
-# try:
-#     file_check(file_path)
-# except Exception as ex:
-#     print(ex)
-#     exit()
-
-# tracklist = read_tracklist(file_path)
-# write_tracklist(tracklist, file_path)
+tracklist = handle_input()
+handle_output(tracklist)
+backup(tracklist)
